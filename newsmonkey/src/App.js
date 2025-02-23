d6a8e6c4aca090338288ce7d14c5caaf9ece635a
Abhishek-km <kumar.kabhi2000@gmail.com>
2025-02-23 22:31:19 +0530

    Added NavBar and created news and newsitemcomponent

--- newsmonkey/src/App.js
+++ newsmonkey/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import NavBar from './Components/NavBar'
+import News from './Components/News'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        This is a class Component
-      </div>
+      <>
+      <NavBar />
+      <News />
+      </>
     )
   }
 }
