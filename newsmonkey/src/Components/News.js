b9aa8e3cad7f1f4408b4e3b20fbf01f091c6aeb5
Abhishek-km <kumar.kabhi2000@gmail.com>
2025-02-24 22:56:23 +0530

    Made Card Panel for Newsitem and displayed them as flex, and passed props for their title and desc

--- newsmonkey/src/Components/News.js
+++ newsmonkey/src/Components/News.js
@@ -4,11 +4,19 @@ import NewsItem from './NewsItem'
 export default class News extends Component {
   render() {
     return (
-      <div>
-        This is a news Component
-        <NewsItem />
-        <NewsItem />    
-        <NewsItem />
+      <div className='container my-3'>
+       <h1>NewsMonkey - Top Headlines</h1>
+       <div className="row">
+         <div className="col-md-4">
+           <NewsItem title = "mytitle" description = "mydesc" />
+         </div>
+         <div className="col-md-4">
+           <NewsItem  title = "mytitle" description = "mydesc"/>
+         </div>
+         <div className="col-md-4">
+           <NewsItem  title = "mytitle" description = "mydesc"/>
+         </div>
+         </div>
       </div>
     )
   }
--- newsmonkey/src/Components/NewsItem.js
+++ newsmonkey/src/Components/NewsItem.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
+    let { title, description } = this.props;
     return (
       <div>
-        This is a news item Component
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://www.jalopnik.com/img/gallery/elon-musk-rejected-ubers-advances-to-partner-on-tesla-robotaxi/l-intro-1740415948.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsDetails" className="btn btn-primary">
+              View More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
