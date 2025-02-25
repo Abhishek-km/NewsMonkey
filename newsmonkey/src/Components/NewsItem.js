2054c20814f56e51dd0cf056c4b4a6b81b0548b9
Abhishek-km <kumar.kabhi2000@gmail.com>
2025-02-25 23:44:47 +0530

    Added a sample response and coded to display content using sample respone

--- newsmonkey/src/Components/NewsItem.js
+++ newsmonkey/src/Components/NewsItem.js
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
 
 export default class NewsItem extends Component {
+
   render() {
-    let { title, description } = this.props;
+    let { title, description, imgUrl, newsUrl } = this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
           <img
-            src="https://www.jalopnik.com/img/gallery/elon-musk-rejected-ubers-advances-to-partner-on-tesla-robotaxi/l-intro-1740415948.jpg"
+            src={imgUrl}
             className="card-img-top"
             alt="..."
           />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/newsDetails" className="btn btn-primary">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} className="btn btn-primary btn-sm">
               View More
             </a>
           </div>
