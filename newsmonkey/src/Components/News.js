b7cd0bf8ff5c6079fd436a60ee67add417fd981f
Abhishek-km <kumar.kabhi2000@gmail.com>
2025-02-27 15:31:50 +0530

    fetching result from API and implementing Previous and next button'

--- newsmonkey/src/Components/News.js
+++ newsmonkey/src/Components/News.js
@@ -8,6 +8,7 @@ export default class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
 
@@ -19,6 +20,26 @@ export default class News extends Component {
     let parsedData = await data.json();
     this.setState({ articles: parsedData.articles });
   }
+
+  handleNextClick = async () => {
+      this.setState({ page: this.state.page + 1 });
+      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=e8d30d9cddd24e7fa9a3f1864d991e13&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({ articles: parsedData.articles });
+  };
+
+  handlePrevClick = async () => {
+    this.setState({ page: this.state.page - 1 });
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=e8d30d9cddd24e7fa9a3f1864d991e13&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles });
+  };
 
   render() {
     return (
@@ -37,6 +58,23 @@ export default class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            class="btn btn-dark"
+            onClick={this.handlePrevClick}
+            disabled={this.state.page <= 1}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            class="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
